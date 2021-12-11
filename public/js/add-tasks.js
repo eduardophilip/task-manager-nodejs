f3d6d7997f9fe659ac770198831ad636ecb4f0d7
Eduard Philip <eduardophilip95@gmail.com>
2021-12-11 18:05:51 +0000

    Modular success message

--- public/js/add-tasks.js
+++ public/js/add-tasks.js
@@ -1,23 +1,8 @@
 import { showTasks } from "./show-tasks.js";
+import { showSuccessMessage, hiddenSuccessMessage } from "./message-success.js";
 const formDOM = document.querySelector('.task-form');
 const taskInputDOM = document.querySelector('.task-input');
 const formAlertDOM = document.querySelector('.task-alert');
-
-const showSuccessMessage = () => {
-
-    taskInputDOM.value = '';
-    formAlertDOM.style.display = 'block';
-    formAlertDOM.textContent = 'Success, tasks went added';
-    formAlertDOM.classList.add("text-success");
-
-}
-
-const hiddenSuccessMessage = () => {
-   setTimeout(() => {
-    formAlertDOM.style.display = 'none';
-    formAlertDOM.classList.remove("text-success");
-   }, 3000) 
-}
 
 formDOM.addEventListener('submit', async (e) => {
 
@@ -35,7 +20,7 @@ formDOM.addEventListener('submit', async (e) => {
         formAlertDOM.style.display = 'block'
         formAlertDOM.innerHTML = `error, please try again`
     }
-
+    taskInputDOM.value = '';
     hiddenSuccessMessage();
 
 });
---
+++ public/js/message-success.js
@@ -0,0 +1,14 @@
+const formAlertDOM = document.querySelector('.task-alert');
+
+export const showSuccessMessage = () => {
+    formAlertDOM.style.display = 'block';
+    formAlertDOM.textContent = 'Success!!';
+    formAlertDOM.classList.add("text-success");
+}
+
+export const hiddenSuccessMessage = () => {
+   setTimeout(() => {
+    formAlertDOM.style.display = 'none';
+    formAlertDOM.classList.remove("text-success");
+   }, 3000) 
+}
